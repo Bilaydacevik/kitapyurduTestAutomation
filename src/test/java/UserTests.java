6b29c02a224e5926b1905cb07c0a83ded34dd2e1
Büşra İlayda Çevik-Köken <bilaydacevik@hotmail.com>
2025-03-01 22:58:13 +0300

    addtoproject

---
+++ src/test/java/UserTests.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserTests {
+    @Test
+    public void LoginSuccessful() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.kitapyurdu.com/index.php?route=account/login");
+        driver.findElement(By.id("login-email")).sendKeys("jamewe1526@egvoo.com");
+        driver.findElement(By.id("login-password")).sendKeys("Test12345678");
+        driver.findElement(By.className("ky-checkbox-input")).click();
+        driver.findElement(By.cssSelector(".ky-btn.ky-btn-orange.w-100.ky-login-btn")).click();
+        Thread.sleep(5000);
+
+        String text = driver.findElements(By.cssSelector(".common-sprite")).get(0).getText();
+        System.out.println(text);
+        Assert.assertEquals(text,"Merhaba Test Test2");
+
+
+    }
+}
