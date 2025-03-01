5c53304f3e4b4a14e9c9f3d143e38460ba14a497
Büşra İlayda Çevik-Köken <bilaydacevik@hotmail.com>
2025-03-01 23:11:58 +0300

    Update UserTests.java

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 public class UserTests {
     @Test
-    public void LoginSuccessful() throws InterruptedException {
+    public void loginSuccessful() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.kitapyurdu.com/index.php?route=account/login");
         driver.findElement(By.id("login-email")).sendKeys("jamewe1526@egvoo.com");
@@ -18,6 +18,18 @@ public class UserTests {
         String text = driver.findElements(By.cssSelector(".common-sprite")).get(0).getText();
         System.out.println(text);
         Assert.assertEquals(text,"Merhaba Test Test2");
+
+
+    }
+    @Test
+    public void loginUnSuccessful() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.kitapyurdu.com/index.php?route=account/login");
+        driver.findElement(By.id("login-email")).sendKeys("jamewe1526@egvoo.com");
+        driver.findElement(By.id("login-password")).sendKeys("Test1234565");
+        driver.findElement(By.className("ky-checkbox-input")).click();
+        driver.findElement(By.cssSelector(".ky-btn.ky-btn-orange.w-100.ky-login-btn")).click();
+        System.out.println(driver.findElement(By.cssSelector("ky-error")).getText());
 
 
     }
