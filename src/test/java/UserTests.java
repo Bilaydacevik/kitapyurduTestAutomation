9e09c3d8faf997f2108de733bad23e48e2da7907
Büşra İlayda Çevik-Köken <bilaydacevik@hotmail.com>
2025-03-02 21:23:25 +0300

    Update UserTests.java

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -2,13 +2,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class UserTests {
+    WebDriver driver; //class içinde tanımlamazsan testlerindeki driverlar hata verir
+    @BeforeMethod
+    public void openBrowser() {
+        driver = new ChromeDriver();
+        driver.get("https://www.kitapyurdu.com/index.php?route=account/login");
+
+    }
     @Test
     public void loginSuccessful() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.kitapyurdu.com/index.php?route=account/login");
+
         driver.findElement(By.id("login-email")).sendKeys("jamewe1526@egvoo.com");
         driver.findElement(By.id("login-password")).sendKeys("Test12345678");
         driver.findElement(By.className("ky-checkbox-input")).click();
@@ -22,17 +30,20 @@ public class UserTests {
 
     }
     @Test
-    public void loginUnSuccessful() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.kitapyurdu.com/index.php?route=account/login");
+    public void loginUnSuccessful() throws InterruptedException {
         driver.findElement(By.id("login-email")).sendKeys("jamewe1526@egvoo.com");
         driver.findElement(By.id("login-password")).sendKeys("Test1234565");
         driver.findElement(By.className("ky-checkbox-input")).click();
         driver.findElement(By.cssSelector(".ky-btn.ky-btn-orange.w-100.ky-login-btn")).click();
-        System.out.println(driver.findElement(By.cssSelector("ky-error")).getText());
-        Assert.assertEquals((driver.findElement(By.cssSelector("ky-error")).getText()),"");
+        Thread.sleep(5000);
+        System.out.println(driver.findElement(By.className("ky-error")).getText());
+        Assert.assertEquals(driver.findElement(By.className("ky-error")).getText(), "E-Posta Adresi ya da şifreniz yanlış. Şifrenizi girerken büyük küçük harf ayrımına dikkat ediniz.");
+
+
+
+    }
+    @AfterMethod
+    public void closeBrowser(){
         driver.quit();
-
-
     }
 }
